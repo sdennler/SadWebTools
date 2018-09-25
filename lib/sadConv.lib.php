9c2709e225fd56e66ef28f1a73f8cc6474b86da7
Samuel Dennler <den@vollbio.de>
2018-09-25 19:50:24 +0200

    sadConv: Code Beautify

--- lib/sadConv.lib.php
+++ lib/sadConv.lib.php
@@ -11,20 +11,20 @@
  * @return string
  */
 function hourDec2hurSex($input){
- $hour = array();
- if(!preg_match('/([0-9]*).([0-9]{1,2})([0-9]?)/', $input, $hour)){
-  return "Error: Input format needs to be like '0.0'.";
- }
- $h = $hour[1];
- $r = ($hour[3] > 4) ? 1 : 0;
- $m = strlen($hour[2]) == 1 ? $hour[2]*10 : $hour[2];
- $m = (int) round(($m+$r)/100*60);
- $hm = (int) ($m/60);
- $h += $hm;
- $m -= $hm*60;
- $h = str_pad($h, 2, '0', STR_PAD_LEFT);
- $m = str_pad($m, 2, '0', STR_PAD_LEFT);
- return $h.':'.$m;
+    $hour = array();
+    if(!preg_match('/([0-9]*).([0-9]{1,2})([0-9]?)/', $input, $hour)){
+        return "Error: Input format needs to be like '0.0'.";
+    }
+    $h = $hour[1];
+    $r = ($hour[3] > 4) ? 1 : 0;
+    $m = strlen($hour[2]) == 1 ? $hour[2]*10 : $hour[2];
+    $m = (int) round(($m+$r)/100*60);
+    $hm = (int) ($m/60);
+    $h += $hm;
+    $m -= $hm*60;
+    $h = str_pad($h, 2, '0', STR_PAD_LEFT);
+    $m = str_pad($m, 2, '0', STR_PAD_LEFT);
+    return $h.':'.$m;
 }
 
 /**
@@ -33,9 +33,9 @@ function hourDec2hurSex($input){
  * @return string
  */
 function hourSex2hourDec($input){
- $hour = array();
- if(!preg_match('/([0-9]{2,}):([0-9]{2})/', $input, $hour)){
-  return "Error: Input format needs to be like '00:00'.";
- }
- return number_format($hour[1]+(round($hour[2]/60, 2)), 2, '.', '');
+    $hour = array();
+    if(!preg_match('/([0-9]{2,}):([0-9]{2})/', $input, $hour)){
+         return "Error: Input format needs to be like '00:00'.";
+    }
+    return number_format($hour[1]+(round($hour[2]/60, 2)), 2, '.', '');
 }
--- sadConv.php
+++ sadConv.php
@@ -26,87 +26,87 @@ if(!isset($_COOKIE['timezone']) || $timezone != $_COOKIE['timezone']){
 
 
 function uStamp2Date($uStamp){
- if (version_compare(PHP_VERSION, '5.0.0', '>=')) {
-  return date('Y-m-d H:i:s O', (int) $uStamp);
- } else {
-  return date('c', $uStamp);
- }
+    if (version_compare(PHP_VERSION, '5.0.0', '>=')) {
+        return date('Y-m-d H:i:s O', (int) $uStamp);
+    } else {
+        return date('c', $uStamp);
+    }
 }
 
 function uStampMillis2Date($uStamp){
- return uStamp2Date($uStamp/1000);
+    return uStamp2Date($uStamp/1000);
 }
 
 function dateTime2Iso8601($timestr){
- return uStamp2Date(strtotime($timestr));
+    return uStamp2Date(strtotime($timestr));
 }
 
 function schoepferExportTranslator($input){
- #return ereg_replace('&quot;&quot;20&quot;&quot;', "\n", $input);
- $input = substr($input, strpos($input, "\n")+1);
- $input = str_replace("\n", ' ', $input);
- $input = str_replace("\r", '', $input);
- $input = str_replace("&quot;", "#", $input);
- $input = preg_replace("/##([0-9]{5,9})##/","\n$1##", $input);
- $input = str_replace("##", "\t", $input);
- $input = str_replace("#", '', $input);
- return $input;
+    #return ereg_replace('&quot;&quot;20&quot;&quot;', "\n", $input);
+    $input = substr($input, strpos($input, "\n")+1);
+    $input = str_replace("\n", ' ', $input);
+    $input = str_replace("\r", '', $input);
+    $input = str_replace("&quot;", "#", $input);
+    $input = preg_replace("/##([0-9]{5,9})##/","\n$1##", $input);
+    $input = str_replace("##", "\t", $input);
+    $input = str_replace("#", '', $input);
+    return $input;
 }
 
 function toIdList($input){
- $input = preg_replace('/[^\d]+/', ',', trim($input));
- $input = trim($input, ',');
- return $input;
+    $input = preg_replace('/[^\d]+/', ',', trim($input));
+    $input = trim($input, ',');
+    return $input;
 }
 
 function toIdListUnique($input){
- $input = implode(',', array_unique(explode(',', toIdList($input))));
- return $input;
+    $input = implode(',', array_unique(explode(',', toIdList($input))));
+    return $input;
 }
 
 function tableColEncode($input){
- $cols = array();
- $match = array();
- $rows = preg_split('@\n@', $input);
- foreach($rows as $row){
-  if(preg_match('@^\| (.*?)[[:space:]]+\|@s', $row, $match)) array_push($cols, $match[1]);
- }
- return implode(', ', $cols);
+    $cols = array();
+    $match = array();
+    $rows = preg_split('@\n@', $input);
+    foreach($rows as $row){
+        if(preg_match('@^\| (.*?)[[:space:]]+\|@s', $row, $match)) array_push($cols, $match[1]);
+    }
+    return implode(', ', $cols);
 }
 
 function getReadableSerializedString($serialized){
- $unserialized = unserialize(stripslashes($serialized));
- if($unserialized === false) $unserialized = 'FALSE or incorrect String!';
- return print_r($unserialized, true);
+    $unserialized = unserialize(stripslashes($serialized));
+    if($unserialized === false) $unserialized = 'FALSE or incorrect String!';
+    return print_r($unserialized, true);
 }
 
 function htmlEncode($string){
- return htmlentities(htmlentities($string)); // Double Encode decoded by the Browser
+    return htmlentities(htmlentities($string)); // Double Encode decoded by the Browser
 }
 
 function scramble_word($word) {
- if(strlen($word) <= 3) return $word;
- else {
-  $new_word = $word;
-  $count = 0;
-  do {
-   $new_word = $word{0} . str_shuffle(substr($word, 1, -1)) . $word{strlen($word) - 1};
-   $count++;
-  } while($new_word == $word && $count < 10);
-  return $new_word;
- }
+    if(strlen($word) <= 3) return $word;
+    else {
+        $new_word = $word;
+        $count = 0;
+        do {
+            $new_word = $word{0} . str_shuffle(substr($word, 1, -1)) . $word{strlen($word) - 1};
+            $count++;
+        } while($new_word == $word && $count < 10);
+        return $new_word;
+    }
 }
 
 function scramble_words($input){
- return preg_replace('/(\w+)/e', 'scramble_word("\1")', $input);
+    return preg_replace('/(\w+)/e', 'scramble_word("\1")', $input);
 }
 
 function tags2lower($input){
- return preg_replace("/(<\/?)(\w+)([^>]*>)/e", "'\\1'.strtolower('\\2').'\\3'", $input);
+    return preg_replace("/(<\/?)(\w+)([^>]*>)/e", "'\\1'.strtolower('\\2').'\\3'", $input);
 }
 
 function tags2upper($input){
- return preg_replace("/(<\/?)(\w+)([^>]*>)/e", "'\\1'.strtoupper('\\2').'\\3'", $input);
+    return preg_replace("/(<\/?)(\w+)([^>]*>)/e", "'\\1'.strtoupper('\\2').'\\3'", $input);
 }
 
 
@@ -114,149 +114,142 @@ function tags2upper($input){
  * Do the conversions *
  **********************/
 
-
- /**
-  * @version 0.6:2018-08-09
-  * @versionTrack 0.3:2009-09-16,0.2:2008-08-08,0.1:2008-06-16,0.1:2005-05-13,0.1:2005-03-02,scratch
-  * @author denner@jobs.ch
-  */
 class sadConv{
 
- var $originalString;
- var $convertetString = false;
- var $method;
- var $decode;
- protected $timeZone;
- var $errorMSG = false;
+    protected $originalString;
+    protected $convertetString = false;
+    protected $method;
+    protected $decode;
+    protected $timeZone;
+    protected $errorMSG = false;
 
- var $methods = array(
-  // Internal Name => Name, encode function, decode function
-  'toIdList'         => array('To ID list', 'toIdList', false),
-  'toIdListUnique'   => array('To ID list (Unique)', 'toIdListUnique', false),
-  'quoted-printable' => array('Quoted-Printable', null, 'quoted_printable_decode'),
-  'base64'           => array('MIME base64', 'base64_encode', 'base64_decode'),
-  'rawurl'           => array('Raw URL (RFC 1738)', 'rawurlencode', 'rawurldecode'),
-  'url'              => array('URL', 'urlencode', 'urldecode'),
-  'utf8'             => array('UTF8', 'utf8_encode', 'utf8_decode'),
-  'md5'              => array('MD5', 'md5', false),
-  'soundex'          => array('Soundex', 'soundex', false),
-  'rot13'            => array('ROT13', 'str_rot13', 'str_rot13'),
-  'scramble_words'   => array('Scramble Words', 'scramble_words', null),
-  'serialize'        => array('Serialize', 'serialize', 'getReadableSerializedString'),
-  'slashes'          => array('Slashes', 'addslashes', 'stripslashes'),
-  'nl2br'            => array('New Line 2 <br />', 'nl2br', null),
-  'htmlentities'     => array('HTML Entites', 'htmlEncode', 'html_entity_decode'),
-  'strip_tags'       => array('Strip HTML Tags', false, 'strip_tags'),
-  'tags2lower'       => array('Tags2lower', 'tags2lower', 'tags2upper'),
-  'strtoupper'       => array('String to upper', 'strtoupper', 'strtolower'),
-  'ucwords'          => array('First in word to upper', 'ucwords', null),
-  'ustamp'           => array('Unix time stamp', 'strtotime', 'uStamp2Date'),
-  'ustampmillis'     => array('Unix time stamp (milliseconds)', null, 'uStampMillis2Date'),
-  'dateTime2Iso8601' => array('DateTime/zone to ISO 8601', 'dateTime2Iso8601', null),
-  'hourdec2hourmin'  => array('Hour decimal 2 Hour sexagesimal', 'hourDec2hurSex', 'hourSex2hourDec'),
-  'schopferTrans'    => array('Sch&ouml;pferTrans', false, 'schoepferExportTranslator'),
-  'tableCols'        => array('Table Col List', 'tableColEncode', null),
-  'countChar'        => array('Count characters', 'strlen', null),
- );
+    protected $methods = array(
+        // Internal Name => Name, encode function, decode function
+        'toIdList'         => array('To ID list', 'toIdList', false),
+        'toIdListUnique'   => array('To ID list (Unique)', 'toIdListUnique', false),
+        'quoted-printable' => array('Quoted-Printable', null, 'quoted_printable_decode'),
+        'base64'           => array('MIME base64', 'base64_encode', 'base64_decode'),
+        'rawurl'           => array('Raw URL (RFC 1738)', 'rawurlencode', 'rawurldecode'),
+        'url'              => array('URL', 'urlencode', 'urldecode'),
+        'utf8'             => array('UTF8', 'utf8_encode', 'utf8_decode'),
+        'md5'              => array('MD5', 'md5', false),
+        'soundex'          => array('Soundex', 'soundex', false),
+        'rot13'            => array('ROT13', 'str_rot13', 'str_rot13'),
+        'scramble_words'   => array('Scramble Words', 'scramble_words', null),
+        'serialize'        => array('Serialize', 'serialize', 'getReadableSerializedString'),
+        'slashes'          => array('Slashes', 'addslashes', 'stripslashes'),
+        'nl2br'            => array('New Line 2 <br />', 'nl2br', null),
+        'htmlentities'     => array('HTML Entites', 'htmlEncode', 'html_entity_decode'),
+        'strip_tags'       => array('Strip HTML Tags', false, 'strip_tags'),
+        'tags2lower'       => array('Tags2lower', 'tags2lower', 'tags2upper'),
+        'strtoupper'       => array('String to upper', 'strtoupper', 'strtolower'),
+        'ucwords'          => array('First in word to upper', 'ucwords', null),
+        'ustamp'           => array('Unix time stamp', 'strtotime', 'uStamp2Date'),
+        'ustampmillis'     => array('Unix time stamp (milliseconds)', null, 'uStampMillis2Date'),
+        'dateTime2Iso8601' => array('DateTime/zone to ISO 8601', 'dateTime2Iso8601', null),
+        'hourdec2hourmin'  => array('Hour decimal 2 Hour sexagesimal', 'hourDec2hurSex', 'hourSex2hourDec'),
+        'schopferTrans'    => array('Sch&ouml;pferTrans', false, 'schoepferExportTranslator'),
+        'tableCols'        => array('Table Col List', 'tableColEncode', null),
+        'countChar'        => array('Count characters', 'strlen', null),
+    );
 
- function sadConv($InputString, $Method, $Way, $TimeZone){
-  $this->originalString = trim($InputString);
-  $this->method = $Method;
-  if($Way == 'decode') $this->decode = true;
-  else $this->decode = false;
-  $this->timeZone = $TimeZone;
- }
+    public function __construct($InputString, $Method, $Way, $TimeZone){
+        $this->originalString = trim($InputString);
+        $this->method = $Method;
+        if($Way == 'decode') $this->decode = true;
+        else $this->decode = false;
+        $this->timeZone = $TimeZone;
+    }
 
- function printImputForm(){
+    public function printImputForm(){
+        $methodList = '';
+        foreach($this->methods as $methodName => $method){
+            $methodList .= '<option value="'.$methodName.'"';
+            if($this->method == $methodName) $methodList .= ' selected="selected"';
+            $methodList .= '>'.$method[0]."</option>\n";
+        }
 
-  $methodList = '';
-  foreach($this->methods as $methodName => $method){
-   $methodList .= '<option value="'.$methodName.'"';
-   if($this->method == $methodName) $methodList .= ' selected="selected"';
-   $methodList .= '>'.$method[0]."</option>\n";
-  }
+        $timeZones = $this->getOptionList(timezone_identifiers_list(), $this->timeZone, false);
 
-  $timeZones = $this->getOptionList(timezone_identifiers_list(), $this->timeZone, false);
+        $form = '<form action="'.$_SERVER['PHP_SELF'].'" method="post" target="_self" accept-charset="ISO-8859-1">'."\n".
+                '<textarea name="InputString" id="InputString" cols="120" rows="12">'.htmlspecialchars(stripslashes($this->originalString)).'</textarea> <label for="InputString" class="textarea">In</label><br />'."\n".
+                '<select name="Method" size="1">'."\n".$methodList.'</select>'."\n".
+                '<div class="options">&nbsp;<input type="Radio" name="Way" value="decode"'.($this->decode?' checked="checked"':'').' /><label for="Way">decode</label>&nbsp;<input type="Radio" name="Way" value="encode"'.(!$this->decode?' checked="checked"':'').' /><label for="Way">encode</label>'."\n".
+                '&nbsp;<input type="Checkbox" name="AsFile" value="yes" /><label for="AsFile">Download&nbsp;result</label></div>'."\n".
+                '&nbsp;<input type="Submit" name="convert" value="Convert">'."\n".
+                '&nbsp;&nbsp;&nbsp;<input type="Button" name="clear" value="Clear" onclick="javascript:document.getElementById(\'InputString\').value=\'\';">'."\n".
+                '<br/><br/><select name="Timezone" size="1">'."\n".$timeZones.'</select>'."\n".
+                "</form>\n";
 
-  $form = '<form action="'.$_SERVER['PHP_SELF'].'" method="post" target="_self" accept-charset="ISO-8859-1">'."\n".
-          '<textarea name="InputString" id="InputString" cols="120" rows="12">'.htmlspecialchars(stripslashes($this->originalString)).'</textarea> <label for="InputString" class="textarea">In</label><br />'."\n".
-          '<select name="Method" size="1">'."\n".$methodList.'</select>'."\n".
-          '<div class="options">&nbsp;<input type="Radio" name="Way" value="decode"'.($this->decode?' checked="checked"':'').' /><label for="Way">decode</label>&nbsp;<input type="Radio" name="Way" value="encode"'.(!$this->decode?' checked="checked"':'').' /><label for="Way">encode</label>'."\n".
-          '&nbsp;<input type="Checkbox" name="AsFile" value="yes" /><label for="AsFile">Download&nbsp;result</label></div>'."\n".
-          '&nbsp;<input type="Submit" name="convert" value="Convert">'."\n".
-          '&nbsp;&nbsp;&nbsp;<input type="Button" name="clear" value="Clear" onclick="javascript:document.getElementById(\'InputString\').value=\'\';">'."\n".
-          '<br/><br/><select name="Timezone" size="1">'."\n".$timeZones.'</select>'."\n".
-          "</form>\n";
+        echo $form;
 
-  echo $form;
+    }
 
- }
+    protected function getOptionList(Array $values, $selected = false, $writeValue = true){
+        $options = '';
+        foreach($values as $key => $value){
+            $options .= '<option';
+            if($writeValue) $options .= ' value="'.$key.'"';
+            else $key = $value;
+            if($selected == $key) $options .= ' selected="selected"';
+            $options .= '>'.$value."</option>\n";
+        }
+        return $options;
+    }
 
- protected function getOptionList(Array $values, $selected = false, $writeValue = true){
-  $options = '';
-  foreach($values as $key => $value){
-   $options .= '<option';
-   if($writeValue) $options .= ' value="'.$key.'"';
-   else $key = $value;
-   if($selected == $key) $options .= ' selected="selected"';
-   $options .= '>'.$value."</option>\n";
-  }
-  return $options;
- }
+    public function convert(){
+        if($this->originalString == '') return false;
+        if(!array_key_exists($this->method, $this->methods)) return false;
 
- function convert(){
-  if($this->originalString == '') return false;
-  if(!array_key_exists($this->method, $this->methods)) return false;
+        $method = $this->methods[$this->method];
+        $methodFunction = $this->decode?$method[2]:$method[1];
 
-  $method = $this->methods[$this->method];
-  $methodFunction = $this->decode?$method[2]:$method[1];
+        if($methodFunction === null){
+            $this->errorMSG = 'Not supported';
+            return false;
+        }
 
-  if($methodFunction === null){
-   $this->errorMSG = 'Not supported';
-   return false;
-  }
+        if($methodFunction === false){
+            $this->errorMSG = 'Not possible';
+            return false;
+        }
 
-  if($methodFunction === false){
-   $this->errorMSG = 'Not possible';
-   return false;
-  }
+        if(is_string($methodFunction)){
+            $this->convertetString = eval("return $methodFunction(\$this->originalString);");
+            if($this->convertetString === null) sad('false!');
+            return true;
+        }
+    } // End function convert
 
-  if(is_string($methodFunction)){
-   $this->convertetString = eval("return $methodFunction(\$this->originalString);");
-   if($this->convertetString === null) sad('false!');
-   return true;
-  }
- } // End function convert
+    public function printOutput(){
+        if(!$this->errorMSG && !($this->convertetString === false)){
+            $form = '<textarea name="OutputString" cols="120" rows="24">'.stripslashes($this->convertetString).'</textarea> <label for="OutputString" class="textarea">Out</label>'."\n";
+            echo $form;
+        }
+        if($this->errorMSG) echo '<b>Error: '.$this->errorMSG.'</b>';
+    }
 
- function printOutput(){
-  if(!$this->errorMSG && !($this->convertetString === false)){
-   $form = '<textarea name="OutputString" cols="120" rows="24">'.stripslashes($this->convertetString).'</textarea> <label for="OutputString" class="textarea">Out</label>'."\n";
-   echo $form;
-  }
-  if($this->errorMSG) echo '<b>Error: '.$this->errorMSG.'</b>';
- }
+    public function sendOutputAsFile(){
+        if(!$this->errorMSG && !($this->convertetString === false)){
+            $output_file = 'sadConved.file';
+            $convertetString = stripslashes($this->convertetString);
 
- function sendOutputAsFile(){
-  if(!$this->errorMSG && !($this->convertetString === false)){
+            header('Pragma: public');
+            header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+            header('Cache-Control: no-store, no-cache, must-revalidate'); // HTTP/1.1
+            header('Cache-Control: pre-check=0, post-check=0, max-age=0'); // HTTP/1.1
+            header('Content-Transfer-Encoding: none');
+            header('Content-Type: application/octetstream; name="'.$output_file.'"'); //This should work for IE & Opera
+            header('Content-Type: application/octet-stream; name="'.$output_file.'"'); //This should work for the rest
+            header('Content-Disposition: inline; filename="'.$output_file.'"');
+            header('Content-length: '.strlen($convertetString));
 
-   $output_file = 'sadConved.file';
-   $convertetString = stripslashes($this->convertetString);
+            echo $convertetString;
+            exit();
+        }
+        if($this->errorMSG) echo '<b>Error: '.$this->errorMSG.'</b>';
+    }
 
-   header('Pragma: public');
-   header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-   header('Cache-Control: no-store, no-cache, must-revalidate'); // HTTP/1.1
-   header('Cache-Control: pre-check=0, post-check=0, max-age=0'); // HTTP/1.1
-   header('Content-Transfer-Encoding: none');
-   header('Content-Type: application/octetstream; name="'.$output_file.'"'); //This should work for IE & Opera
-   header('Content-Type: application/octet-stream; name="'.$output_file.'"'); //This should work for the rest
-   header('Content-Disposition: inline; filename="'.$output_file.'"');
-   header('Content-length: '.strlen($convertetString));
-
-   echo $convertetString;
-   exit();
-  }
-  if($this->errorMSG) echo '<b>Error: '.$this->errorMSG.'</b>';
- }
 } // End class sadConv
 
 
